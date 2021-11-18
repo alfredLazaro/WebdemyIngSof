b9f5777424e38e66953987dddea6512380458ee4
maurihv13 <huaytamauricio1@gmail.com>
2021-11-18 15:19:11 -0400

    Cambio en descrip para prueba deploy

--- app/components/body-components/Body.jsx
+++ app/components/body-components/Body.jsx
@@ -17,7 +17,7 @@ const Body =()=>{
                     <p> 
                         Esta pagina esta pensada para el aprendizaje de herramientas 
                         tecnológicas y fomentar el aprendizaje en todas las personas 
-                        que deseen aprender   
+                        que deseen aprender  CAMBIO MAU AAAAA
                     </p>
                 </div>        
             </div>        
