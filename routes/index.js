31ee8672e3e27f0b0ed638e09e55e9cc226b9e49
fred <alfredoLazaro999@gmail.com>
2021-11-18 11:56:45 -0400

    hice ultimos arreglos en las consultas deberia aparecer bien

--- routes/index.js
+++ routes/index.js
@@ -10,27 +10,13 @@ router.get('/', async (req, res) => {
     res.send(cursos); //muestra la consulta en la pagina
 });
 
-router.get('/curso', async (req,res)=>{
-    const repetidos= await pool.query('SELECT * FROM curso ORDER BY inscritos DESC,fechaCreacion DESC');
-    res.send(repetidos);
-});
 
 router.get('/cursos', async (req,res)=>{
-    const repetidos= await pool.query('SELECT curso.id_curso, curso.nombre as nombrecurso, curso.imagen, curso.inscritos, curso.created_at, etiqueta.nombre as nombreEtiqueta, usuario.nombres as nomT, usuario.apellidos as apellT FROM curso, curso_has_etiqueta, etiqueta, tutor, usuario WHERE curso.id_curso = curso_has_etiqueta.curso_id_curso and curso_has_etiqueta.etiqueta_id_etiqueta = etiqueta.id_etiqueta and curso.tutor_id_tutor = tutor.id_tutor and usuario.id_usuario = tutor.usuario_id_usuario ORDER BY inscritos desc,created_at desc');
+    const repetidos= await pool.query('SELECT curso.id_curso, curso.nombre as nombreCurso, curso.imagen, curso.inscritos, curso.created_at, etiqueta.nombre as nombreEtiqueta, usuario.nombres as nomT, usuario.apellidos as apellT FROM curso, curso_has_etiqueta, etiqueta, tutor, usuario WHERE curso.id_curso = curso_has_etiqueta.curso_id_curso and curso_has_etiqueta.etiqueta_id_etiqueta = etiqueta.id_etiqueta and curso.tutor_id_tutor = tutor.id_tutor and usuario.id_usuario = tutor.usuario_id_usuario ORDER BY inscritos desc,created_at desc');
     res.send(repetidos);
 });
 
-router.get('/cursoU',async (req,res)=>{
-    //const idCurso= //parte del frontend obtenerlo
-    const cursoUnico = await pool.query('SELECT * FROM Modulo join CURSO where id_curso=CURSO_id_curso and id_curso = ?',5113);
-    res.send(cursoUnico);
-});
 
-router.get('/etiqueta', async (req,res)=>{
-    //const etiq=
-    const cursoEti = await pool.query('SELECT CURSO.nombre,CURSO.imagen,CURSO.inscritos,CURSO.descripcion,CURSO.requisitos,CURSO.duracion,CURSO.fechaCreacion FROM Etiqueta as E join CURSO Join   where id_curso = curso_id_curso and id_etiqueta=etiqueta_id_etiqueta and E.nombre= ?', 'python');
-    res.send(cursoEti);CURSO_has_etiqueta
-});
 router.get('/etiqueta/:palabra', async (req,res)=>{
     const { palabra } = req.params;
     const cursoEti = await pool.query('SELECT curso.nombre,curso.imagen,curso.inscritos,curso.descripcion,curso.requisitos,curso.duracion,curso.fechaCreacion FROM etiqueta as E join curso Join curso_has_etiqueta  where id_curso = curso_id_curso and id_etiqueta=etiqueta_id_etiqueta and E.nombre  ?',[palabra], (err,rows,fields) => {
@@ -72,7 +58,7 @@ router.get('/:id/modulos', async (req, res) => {
 
 router.get('/:id/etiquetas', async (req, res) => {
     const { id } = req.params;
-    const cursos = await pool.query('SELECT E.nombre FROM etiqueta as E Join curso  Join curso_has_curso WHERE id_curso = curso_id_curso and id_etiqueta=etiqueta_id_etiqueta and id_curso= ?', [id], (err,rows,fields) => {
+    const cursos = await pool.query('SELECT E.nombre FROM etiqueta as E Join curso  Join curso_has_etiqueta WHERE id_curso = curso_id_curso and id_etiqueta=ETIQUETA_id_etiqueta and id_curso= ?', [id], (err,rows,fields) => {
         if(!err){
             res.json(rows);
         }else{
