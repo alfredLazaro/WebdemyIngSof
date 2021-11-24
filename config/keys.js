c2a6821be3dc7703873fa28b205aa3b562f79d05
Viclo <ivangra85@gmail.com>
2021-11-23 21:57:00 -0400

    solución bug 93

--- config/keys.js
+++ config/keys.js
@@ -1,12 +1,18 @@
 module.exports = {
 
-    database: {
+    /*database: {
 
         host: 'bnzejryfzccllt1iv6cd-mysql.services.clever-cloud.com',
         user: 'uwniakdgootbcctr',
         password: 'pl7TQyNVrNog321QJICH',
         database: 'bnzejryfzccllt1iv6cd'
 
-    }
+    }*/
 
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: '85201674$GarrA',
+        database: 'wdemy'
+    }
 }; 
