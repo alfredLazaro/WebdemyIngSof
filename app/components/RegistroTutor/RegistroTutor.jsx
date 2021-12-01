4269c5f4a558812afba298efa1bfc2eeccf3a216
javierFili <javierfiligranaagreda@gmail.com>
2021-12-01 17:00:06 -0400

    "cambiando los estilos"

--- app/components/RegistroTutor/RegistroTutor.jsx
+++ app/components/RegistroTutor/RegistroTutor.jsx
@@ -2,6 +2,25 @@ import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import "./RegistroTutor.css";
 class RegistroTutor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      texTrabActual: "",
+      textTrabAnter: "",
+      textTrabExtra: "",
+      link1: "",
+      link2: "",
+      link3: "",
+      link4: "",
+    };
+    this.changeTrabActual = this.changeTrabActual.bind(this);
+    this.changeTrabAnteri = this.changeTrabAnteri.bind(this);
+    this.changeTrabExtra = this.changeTrabExtra.bind(this);
+  }
+  changeTrabActual() {}
+  changeTrabAnteri() {}
+  changeTrabExtra() {}
+
   render() {
     return (
       <html lang="en">
@@ -31,31 +50,83 @@ class RegistroTutor extends Component {
               id="formRegisInfoPer"
               class="w3-padding w3-left  w3-card-4 w3-white"
             >
-              <h3>  Informacion personal</h3>
-              <ul><input type="checkbox" /></ul>
-              <ul><input type="checkbox" /></ul>
-              <ul><input type="checkbox" /></ul>
-              <ul><input type="checkbox" /></ul>
-              <ul><input type="checkbox" /></ul>
-              <ul><input type="checkbox" /></ul>
+              <h3> Informacion personal</h3>
+              <ul>
+                <input type="checkbox" />
+              </ul>
+              <ul>
+                <input type="checkbox" />
+              </ul>
+              <ul>
+                <input type="checkbox" />
+              </ul>
+              <ul>
+                <input type="checkbox" />
+              </ul>
+              <ul>
+                <input type="checkbox" />
+              </ul>
+              <ul>
+                <input type="checkbox" />
+              </ul>
             </form>
+            <ul>mensaje de error</ul>
             <form
               id="formRegisLinks"
               class="w3-padding w3-right  w3-card-4 w3-white"
             >
               <h3>Links</h3>
-              <input type="text" />
-              
+              <ul>
+                <input
+                  type="text"
+                  value={this.state.link1}
+                />
+              </ul>
+              <ul>mensaje de error</ul>
             </form>
             <form
               id="formRegisTrab"
               class="w3-padding w3-center  w3-card-4 w3-white"
             >
               <h3>Trabajo</h3>
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
+              <ul>
+                <input
+                  id="inputTrabajo"
+                  type="text"
+                  placeholder="            Trabajo anterior"
+                  value={this.state.textTrabAnter}
+                  onChange={this.changeTrabAnteri}
+                />
+              </ul>
+              <ul>mensaje de error</ul>
+              <ul>
+                <input
+                  id="inputTrabajo"
+                  type="text"
+                  placeholder="            Trabajo actual "
+                  value={this.state.texTrabActual}
+                  onChange={this.changeTrabActual}
+                />
+              </ul>
+              <ul>
+                <h10>Debe rellenar este campo El texto debe ser menor a 50 caracteres</h10>
+              </ul>
+              <ul>El texto debe ser menor a 50 caracteres</ul>
+              <ul> El texto debe contener más de 15 caracteres </ul>
+              <ul> No se aceptan caracteres especiales.</ul>
+              <ul>
+                <input
+                  id="inputTrabajo"
+                  type="text"
+                  placeholder="            Trabajo extra( si lo hubiera)"
+                  value={this.state.textTrabExtra}
+                  onChange={this.changeTrabExtra}
+                />
+              </ul>
+              <ul>mensaje de error</ul>
             </form>
+            <ul>mensaje de error</ul>
+            <button id="btnRegistroTutor">Registrarse</button>
           </form>
         </body>
       </html>
